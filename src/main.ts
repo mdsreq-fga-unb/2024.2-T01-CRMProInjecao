824c7b60988bd786ea6c482c80c9b41007c81f4b
viniciusvieira00 <vncvieira00@gmail.com>
2025-01-21 02:01:02 -0300

    feat(app): enhance global configuration with validation pipes, CORS, and versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 3000);
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [''],
+    prefix: '',
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  await app.listen(process.env.APP_PORT || '8000');
 }
 bootstrap();
