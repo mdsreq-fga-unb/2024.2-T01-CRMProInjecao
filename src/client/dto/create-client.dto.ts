24fdf7635b488ada8f0a9041e1b2a8f80feae850
Pedro Braga <prometeuslbo@gmail.com>
2025-01-20 21:42:12 -0300

    feat: add CPF validation to client creation DTO

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,6 +1,9 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString, Matches } from 'class-validator';
 
 export class CreateClientDto {
+  @IsString()
+  @Matches(/^[0-9]{11}$/)
+  cpf: string;
   @IsString()
   name: string;
 
