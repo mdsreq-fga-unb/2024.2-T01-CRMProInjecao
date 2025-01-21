5f589f72ca765372dd1d00ce5e39c71680b2fe08
viniciusvieira00 <vncvieira00@gmail.com>
2025-01-21 02:01:06 -0300

    refactor(vehicle): update controller methods for consistency and usability

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
@@ -16,27 +17,30 @@ export class VehicleController {
   constructor(private readonly vehicleService: VehicleService) {}
 
   @Post()
-  create(@Body() createVehicleDto: CreateVehicleDto) {
+  async create(@Body() createVehicleDto: CreateVehicleDto) {
     return this.vehicleService.create(createVehicleDto);
   }
 
   @Get()
-  findAll() {
-    return this.vehicleService.findAll();
+  async findAll(@Query('clientCPF') clientCPF?: string) {
+    return this.vehicleService.findAll(clientCPF);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.vehicleService.findOne(+id);
+  @Get(':licensePlate')
+  async findOne(@Param('licensePlate') licensePlate: string) {
+    return this.vehicleService.findOne(licensePlate);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
-    return this.vehicleService.update(+id, updateVehicleDto);
+  @Patch(':licensePlate')
+  async update(
+    @Param('licensePlate') licensePlate: string,
+    @Body() updateVehicleDto: UpdateVehicleDto,
+  ) {
+    return this.vehicleService.update(licensePlate, updateVehicleDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.vehicleService.remove(+id);
+  @Delete(':licensePlate')
+  async remove(@Param('licensePlate') licensePlate: string) {
+    return this.vehicleService.remove(licensePlate);
   }
 }
