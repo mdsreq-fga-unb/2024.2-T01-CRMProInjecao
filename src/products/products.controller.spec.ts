f80b40ec5b0df531c015a8b6c6b7f9806f5e4374
Pedro Braga <prometeuslbo@gmail.com>
2025-01-31 00:58:31 -0300

    test(products): enhance unit tests for ProductsController and ProductsService with create functionality and mock data
    Co-authored-by: Bruno Bragança dos Reis <62809606+BrunoBReis@users.noreply.github.com>

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,20 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './entities/product.entity';
+
+const mockProduct: Product = {
+  id: '1',
+  name: 'Test Product',
+  description: 'Test Description',
+  brand: 'Test Brand',
+  costPrice: 100,
+  sellPrice: 150,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null,
+  categories: [
+    {
+      id: '1',
+      name: 'test',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+    },
+    {
+      id: '2',
+      name: 'grapes',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+    },
+  ],
+};
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
+    service = module.get<ProductsService>(ProductsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a product', async () => {
+      const createProductDto: CreateProductDto = {
+        name: 'Test Product',
+        costPrice: 100,
+        sellPrice: 150,
+        categories: ['test', 'grapes'],
+        brand: 'Test Brand',
+        description: 'Test Description',
+      };
+      jest.spyOn(service, 'create').mockResolvedValue(mockProduct);
+
+      expect(await controller.create(createProductDto)).toBe(mockProduct);
+    });
   });
 });
