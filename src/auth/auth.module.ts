d0f7ab803ae14ff8bdf51a68811f2e27895efe8a
viniciusvieira00 <vncvieira00@gmail.com>
2025-02-06 12:13:45 -0300

    fix(auth): correct import path for UserModule in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '../user/user.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
 import { EmailModule } from 'src/email/email.module';
