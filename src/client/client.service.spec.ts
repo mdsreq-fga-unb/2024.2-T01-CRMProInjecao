e7016c37b58f11cf0ef8fec7d15bd3c0bde1d089
viniciusvieira00 <vncvieira00@gmail.com>
2025-02-06 12:14:37 -0300

    test(client): mock repository dependencies in ClientService tests

--- src/client/client.service.spec.ts
+++ src/client/client.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientService } from './client.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Client } from './entities/client.entity';
+import { Repository } from 'typeorm';
 
 describe('ClientService', () => {
   let service: ClientService;
+  let mockClientRepository: jest.Mocked<Repository<Client>>;
 
   beforeEach(async () => {
+    mockClientRepository = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+      softDelete: jest.fn(),
+    } as any;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientService],
+      providers: [
+        ClientService,
+        { provide: getRepositoryToken(Client), useValue: mockClientRepository },
+      ],
     }).compile();
 
     service = module.get<ClientService>(ClientService);
